317a3cff9676dafe6d6e16742442acfa104f466f
Mena Perez, James Steve <jsmena5@espe.edu.ec>
2025-01-11 18:11:56 -0500

    Add files via upload

---
+++ Solucionario/Continentes/Asia.php
@@ -0,0 +1,26 @@
+<?php
+
+// MATRICES ASOCIATIVAS	           
+$Asia= array (
+       
+            
+       'CHINA' =>array('SHANGAI' => ["HANGPU","XUHUI","JING'AN","PUDONG"],
+                            'PEKIN' => ["DONGCHENG","CHAOYANG","HAIDIAN","XICHENG","FENGTAI","SINJINGSHAN"],
+                                 'HONG KONG' => ["TAI PO","SAI KUNG","KWAI TSING","NORTH"],                                
+                            ),
+             
+      'INDIA' =>array('NUEVA DELHI' => ["NEW DELHI","SOUTH DELHI","WEST DELHI","EAST DELHI"],
+                                   'MUMBAI' => ["SOUTH MUMBAI","CENTER MUMBAI","WESTERN MUMBAI","EASTERN MUMBAI"],
+                                'BANGALORE' => ["CENTRAL BANGALORE","NORTH BANGALORE","WEST BANGALORE","EAST BANGALORE"]),
+              
+     'JAPON' => array('TOKIO' => ["ADACHI","ARAKAWA","CHIYODA","KITA","MEGURO"],
+                            'KIOTO' => ["FUSHIMI","KITA","SAKYO","UKYO","SHIMOGYO"],
+                            'OSAKA' => ["ABENO","JOTO","NISHI","SUMINOE","TSURUMI"],
+                            
+       )
+
+);
+       
+
+
+?>
